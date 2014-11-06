eb7bcacbbd227974aa9074715eb3e526751d7333
Hans Meine <hans_meine@gmx.net>
2014-11-06 16:54:24 +0100

    add commandline arguments and —help

--- cli_to_json.py
+++ cli_to_json.py
@@ -1,19 +1,25 @@
-import sys, os
+import sys, os, argparse
 import simplejson
 import cli_modules
 
-basedir, = sys.argv[1:]
+parser = argparse.ArgumentParser(description = 'create JSON description from CLI modules')
+parser.add_argument('base_directory', nargs = '+',
+                    help = 'directories (at least one) in which to search for CLI module executables')
+parser.add_argument('--json_filename', '-o', type = argparse.FileType('w'), default = sys.stdout)
+
+args = parser.parse_args()
 
 INDEX_ATTRIBUTES = 'name title version category description contributor acknowledgements documentation_url license'.split()
 
 docs = []
 
-for exe_filename in cli_modules.listCLIExecutables(basedir):
-    sys.stderr.write('processing %s...\n' % (os.path.basename(exe_filename), ))
-    cli = cli_modules.CLIModule(exe_filename)
+for basedir in args.base_directory:
+    for exe_filename in cli_modules.listCLIExecutables(basedir):
+        sys.stderr.write('processing %s...\n' % (os.path.basename(exe_filename), ))
+        cli = cli_modules.CLIModule(exe_filename)
 
-    docs.append(dict((attr, getattr(cli, attr))
-                     for attr in INDEX_ATTRIBUTES))
+        docs.append(dict((attr, getattr(cli, attr))
+                         for attr in INDEX_ATTRIBUTES))
 
-simplejson.dump(docs, sys.stdout, indent = '  ')
-sys.stdout.write('\n')
+simplejson.dump(docs, args.json_filename, indent = '  ')
+args.json_filename.write('\n')
--- index_from_json.py
+++ index_from_json.py
@@ -1,13 +1,21 @@
-import sys
+import sys, argparse
 import simplejson
 import elasticsearch
 
-json_filename, source = sys.argv[1:]
+parser = argparse.ArgumentParser(description = 'update elasticsearch index from JSON description of CLI modules')
+parser.add_argument('json_filename', type = argparse.FileType('r'),
+                    help = 'name of JSON file as created by cli_to_json.py')
+parser.add_argument('source', help = "identifier for the source "
+                    "(e.g. 'Slicer' or 'nifty-reg') of this set of CLI modules "
+                    "(will also be used to remove all documents from this source "
+                    "from the Elasticsearch index if they are not in the JSON anymore)")
+
+args = parser.parse_args()
 
 INDEX = 'cli'
 DOC_TYPE = 'cli'
 
-docs = simplejson.load(file(json_filename))
+docs = simplejson.load(args.json_filename)
 
 es = elasticsearch.Elasticsearch()
 
@@ -15,14 +23,14 @@ existing = [doc['_id'] for doc in
             es.search(INDEX, DOC_TYPE, body = dict(
                 query = dict(
                     term = dict(
-                        source = source)
+                        source = args.source)
                     )),
                 fields = ['_id'],
                 size = 100000)['hits']['hits']]
 
 for doc in docs:
-    doc['source'] = source
-    doc_id = '%s:%s' % (source, doc['name'])
+    doc['source'] = args.source
+    doc_id = '%s:%s' % (args.source, doc['name'])
 
     try:
         old = es.get(INDEX, doc_id, DOC_TYPE)
@@ -38,6 +46,5 @@ for doc in docs:
             sys.stdout.write("leaving '%s' alone, no change...\n" % doc_id)
 
 for doc_id in existing:
-    sys.stdout.write("removing '%s', which is no longer in JSON..." % doc_id)
+    sys.stdout.write("removing '%s', which is no longer in the '%s' JSON..." % (doc_id, args.source))
     es.delete(INDEX, DOC_TYPE, doc_id)
-    
